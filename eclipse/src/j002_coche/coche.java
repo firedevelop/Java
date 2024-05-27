2a9b6eefcc8cd3f1a7a3f9c18597bf415b85ed3a
fireDevelop <firedevelop.com@gmail.com>
2024-05-27 12:16:13 +0200

    .
    .

---
+++ eclipse/src/j002_coche/coche.java
@@ -0,0 +1,7 @@
+package j002_coche;
+
+public class coche {
+    public static void main(String[] args) {
+        System.out.println("coche6");
+    }
+}
---
+++ intelliJ/src/j001_static/Main.java
@@ -0,0 +1,6 @@
+package j001_static;
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!6");
+    }
+}
