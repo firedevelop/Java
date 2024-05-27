9452bf7ec617e11f263fc50c2b845506de84f6d2
fireDevelop <firedevelop.com@gmail.com>
2024-05-27 18:03:08 +0200

    .

--- intelliJ/src/j001_static/Main.java
+++
@@ -1,6 +0,0 @@
-package j001_static;
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!6");
-    }
-}
---
+++ intelliJ/src/j002_static_methods/Main.java
@@ -0,0 +1,9 @@
+package j002_static_methods;
+
+public class Main {
+    public static void main(String[] args) {
+        // Llamada al método estático sumar de la clase Calculadora
+        int resultado = Calculadora.sumar(5, 3);
+        System.out.println("La suma es: " + resultado);
+    }
+}
