07cc268524648e1522bf744d6f246356c6e15f1b
fireDevelop <firedevelop.com@gmail.com>
2024-06-28 23:39:06 +0200

    .
    .

---
+++ intelliJ/src/J013_Protected/Main.java
@@ -0,0 +1,10 @@
+package J013_Protected;
+
+import J015_Protected.ClassB;
+
+public class Main{
+    public static void main(String[] args) {
+        ClassB objB = new ClassB();
+        objB.testAccess();
+    }
+}
