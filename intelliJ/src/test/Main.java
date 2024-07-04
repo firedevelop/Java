a4ca281d22af1020b8c9bd2f983426b2df17860d
fireDevelop <firedevelop.com@gmail.com>
2024-07-04 18:30:59 +0200

    .
    .

--- intelliJ/src/test/Main.java
+++ intelliJ/src/test/Main.java
@@ -1,22 +1,21 @@
 package test;
 
-import java.util.Scanner;
-
 public class Main{
     public static void main(String[] args) {
-        int[] array = new int[3];
-        int num;
-        Scanner sc = new Scanner(System.in);
-        System.out.println("num1: ");
-        array[0] = sc.nextInt();
-
-        System.out.println("num2");
-        array[1] = sc.nextInt();
-
-        array[2] = array[0] + array[1];
-        System.out.println(array[0] + " + " + array[1] + " = " + array[2]);
+        int[] array = {};
 
 
+
+        
+        int a = array.hashCode();
+        System.out.println(a);
+
+        int[] a1 = {};
+        int b = a1.hashCode();
+
+        int[] a2 = {};
+        int h2 = a2.hashCode();
+        System.out.println(h2);
 
 
     }
